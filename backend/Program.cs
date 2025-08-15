7b4bf07a0a5cfd6128b636b5605dd042ec57c110
Paavo Penttila <thxmuffe@hotmail.com>
2025-08-15 13:33:32 +0300

    cleanup code

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,17 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!"); // Using "anonomous functions"
-app.MapGet("hellous/", HelloMehthod);
+app.MapGet("hellous/", GetHello);
 
 app.Run();
 
-string HelloMehthod()
+string GetHello()
 {
     var helloFolder = new DirectoryInfo(Directory.GetCurrentDirectory());
     var helloPath = Path.Combine(helloFolder.FullName, "hello.txt");
-
-    // Alternative, shorter syntaxc (same result)
-    var helloPath2 = Path.Combine(Directory.GetCurrentDirectory(), "hello.txt");
 
     // print to console absolute path (Fullname)
     Console.WriteLine($"Reading hello from: {helloPath}");
