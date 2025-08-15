ff5f4f9cc9b67e47025e38c40b5719dbb5fe008b
Paavo Penttila <thxmuffe@hotmail.com>
2025-08-15 12:23:23 +0300

    Add HelloMethod to be used with hellous-endpoint

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "Hello World!"); // Using "anonomous functions"
+app.MapGet("hellous/", HelloMehthod);
 
 app.Run();
+
+string HelloMehthod()
+{
+    // Soon we add code, which reads the message from a file...
+    var message = "Moicca";
+    return message + "!";
+}
